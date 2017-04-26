18953db83a0603084cbf31be8115b1ffb1140189
Cameron Dubas <camerondubas@gmail.com>
2017-04-26 23:34:31 +0200

    Fix Incorrect path to Help Action

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,7 +1,7 @@
 import send from './send.action';
 import greetingAction from './greeting.action';
 import weatherAction from './weather/weather.action';
-import helpAction from './weather/help.action';
+import helpAction from './help.action';
 
 export default {
   send,
