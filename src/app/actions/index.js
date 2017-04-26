6ad52283d7585cf6e91b97cc2c19cf6e29afd10f
Cameron Dubas <camerondubas@gmail.com>
2017-04-26 22:32:43 +0200

    Add help action, remove unsused code

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,9 +1,11 @@
 import send from './send.action';
 import greetingAction from './greeting.action';
 import weatherAction from './weather/weather.action';
+import helpAction from './weather/help.action';
 
 export default {
   send,
   greetingAction,
-  weatherAction
+  weatherAction,
+  helpAction
 };
